ad95eab9e3efc3978156842dd52a25733d8a4e9f
AAK05 <adrien.kusuma@gmail.com>
2022-10-06 18:46:30 +0700

    Added samplevismachine function to fouriermain to visualise winding machine of Fourier transformations

--- fouriermain.py
+++ fouriermain.py
@@ -24,5 +24,25 @@ def fouriertrans(file,offset,duration):
         plt.show()
     return freqs,fourierspace
 
-audiosignal("2022-03-27 20-49-06.wav",1.9,1)
-fouriertrans("2022-03-27 20-49-06.wav",1.9,1)
+def samplevismachine(freq=1):
+    x = np.linspace(0,10,1000)
+    y = [np.sin(4*np.pi*i) for i in x]
+    plt.plot(x,y)
+    plt.show()
+    #yhat = [i*np.exp(-2*1j*np.pi*freq) for i in y]
+    yhat = []
+    index = 0
+    for i in y:
+       yhatel = i*np.exp(-2*1j*np.pi*freq*(index/(len(y)-1)))
+       yhat.append(yhatel)
+       index += 1
+    yhatr = [np.real(i) for i in yhat]
+    yhatim = [np.imag(i) for i in yhat]
+    plt.plot(yhatr,yhatim,linewidth=0.3)
+    plt.grid()
+    plt.show()
+
+
+#audiosignal("2022-03-27 20-49-06.wav",1.9,1)
+#fouriertrans("2022-03-27 20-49-06.wav",1.9,1)
+samplevismachine(2)
