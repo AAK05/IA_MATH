7e136ac553727f7fd360bfe193a0f0f84e3e1410
AAK05 <adrien.kusuma@gmail.com>
2022-10-07 09:09:01 +0700

    Modified fouriermain.py

--- fouriermain.py
+++ fouriermain.py
@@ -25,24 +25,33 @@ def fouriertrans(file,offset,duration):
     return freqs,fourierspace
 
 def samplevismachine(freq=1):
-    x = np.linspace(0,10,1000)
-    y = [np.sin(4*np.pi*i) for i in x]
+    #x = np.linspace(0,10*17/6,1000000)
+    x = np.linspace(0,np.pi,1000000)
+    y = [np.cos(6*np.pi*i) for i in x]
     plt.plot(x,y)
     plt.show()
     #yhat = [i*np.exp(-2*1j*np.pi*freq) for i in y]
     yhat = []
     index = 0
     for i in y:
-       yhatel = i*np.exp(-2*1j*np.pi*freq*(index/(len(y)-1)))
+       yhatel = i*np.exp(-2*1j*np.pi*freq*(index/(len(y))))
        yhat.append(yhatel)
        index += 1
+    yhatsum = sum(yhat)
+    print(yhatsum)
     yhatr = [np.real(i) for i in yhat]
     yhatim = [np.imag(i) for i in yhat]
+    plt.xlim(-1,1)
+    plt.ylim(-1,1)
     plt.plot(yhatr,yhatim,linewidth=0.3)
     plt.grid()
     plt.show()
 
+    xf = rfftfreq(len(x),len(x)*(6/17))
+    yf = rfft(y)
+    plt.plot(xf,np.abs(yf))
+    plt.show()
 
 #audiosignal("2022-03-27 20-49-06.wav",1.9,1)
 #fouriertrans("2022-03-27 20-49-06.wav",1.9,1)
-samplevismachine(2)
+samplevismachine(0.1)
